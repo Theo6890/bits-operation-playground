96491a7cd4b40193a17161643d19bf1fbdccac5e
Théo RICHARD <mail@rcode.tech>
2023-01-09 16:39:07 +0100

    feat: constant MASK for packed user data

--- src/BitwiseOperation.sol
+++ src/BitwiseOperation.sol
@@ -6,5 +6,14 @@ pragma solidity ^0.8.13;
  *         in many different cases.
  */
 contract BitwiseOperation {
-    
+    /** @dev Contains packed user data:
+     * bytes8 pseudo; uint8 day; uint8 month; uint8 year; address user
+     */
+    bytes32 public userData;
+
+    bytes32 public constant PSEUDO_MASK =
+        bytes32(uint256(0xffffffffffffffff << 192));
+    bytes32 public constant DAY_MASK = bytes32(uint256(0xff << 184));
+    bytes32 public constant MONTH_MASK = bytes32(uint256(0xff << 176));
+    bytes32 public constant YEAR_MASK = bytes32(uint256(0xffff << 160));
 }
