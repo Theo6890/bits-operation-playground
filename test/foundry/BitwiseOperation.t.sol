938dcd45fbb9c4cd12e6afb736331858254842e5
Théo RICHARD <mail@rcode.tech>
2023-01-09 18:10:47 +0100

    chore(test): fix import

--- test/foundry/BitwiseOperation.t.sol
+++ test/foundry/BitwiseOperation.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-import "../../src/BitwiseOperation.sol";
+import {BitwiseOperation} from "../../src/BitwiseOperation.sol";
 
 contract BitwiseOperationTest is Test {
     BitwiseOperation public bit;
