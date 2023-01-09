a3a32b4585dc8013877110656d9ebf308f92ae69
Théo RICHARD <mail@rcode.tech>
2023-01-09 16:39:14 +0100

    test: constant MASK for packed user data

--- test/foundry/BitwiseOperation.t.sol
+++ test/foundry/BitwiseOperation.t.sol
@@ -15,7 +15,35 @@ contract BitwiseOperationTest is Test {
     /*//////////////////////////////////////////////////////////////
                                  BASIC ATTRIBUTES
     //////////////////////////////////////////////////////////////*/
-    function testTruthy() public {
-        assertTrue(true);
+    function test_constant_PseudoMask() public {
+        assertEq(
+            uint256(bit.PSEUDO_MASK()),
+            // bytes8 at 1 for pseudo; bytes24 at 0
+            0xffffffffffffffff000000000000000000000000000000000000000000000000
+        );
+    }
+
+    function test_constant_DayMask() public {
+        assertEq(
+            uint256(bit.DAY_MASK()),
+            // bytes8 at 0 for pseudo; bytes1 at 1 for day; bytes23 at 0
+            0x0000000000000000ff0000000000000000000000000000000000000000000000
+        );
+    }
+
+    function test_constant_MonthMask() public {
+        assertEq(
+            uint256(bit.MONTH_MASK()),
+            // bytes8 at 0 for pseudo; bytes1 at 0 for day; bytes1 at 1 for month; 22 bytes at 0
+            0x000000000000000000ff00000000000000000000000000000000000000000000
+        );
+    }
+
+    function test_constant_YearMask() public {
+        assertEq(
+            uint256(bit.YEAR_MASK()),
+            // bytes8 at 0 for pseudo; bytes1 at 0 for day; bytes1 at 1 for month; bytes2 at 1 for year;
+            0x00000000000000000000ffff0000000000000000000000000000000000000000
+        );
     }
 }
